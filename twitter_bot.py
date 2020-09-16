29e78374c4f500a553f81878a1c615190f6da8c4
Naveen Nammi <71389724+naveen-colab@users.noreply.github.com>
2020-09-16 19:23:46 +0530

    Create twitter_bot.py

---
+++ twitter_bot.py
@@ -0,0 +1,28 @@
+import tweepy  #authentication
+import time
+import os
+def authentication():
+  consumer_key=os.getenv("consumer_key")
+  consumer_secret=os.getenv("consumer_secret")
+  access_token=os.getenv(" access_token")
+  access_token_secret=os.getenv("access_token_secret")
+  
+  auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+  auth.set_access_token(access_token, access_token_secret)
+  api = tweepy.API(auth)
+  api.verify_credentials()
+  print("api created")
+  return api
+ 
+ def new_name(user):
+  emoji_dict={0:"0️⃣",1:"1️⃣",2:"2️⃣",4:"4",5:"5",6:"6",7:"7",8:"8",9:"9"}
+  emoji_list ="".join([emoji_dict[int(i)] for i in str(user.followers_count) if int(i) in emoji_dict.keys()])
+  return emoji_list
+
+api = authentication()
+while True:
+  user = api.get_user("@Chandra34570619")
+  api .update_profile(name = f"itsmenaveen|{new_name(user)}")
+  print( f"chandrateja|{new_name}")
+  print("waiting to refresh")
+  time.sleep(60)
