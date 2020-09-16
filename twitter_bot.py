42b2230ee80bc8a7730e997b17dc5e07173d4f33
Naveen Nammi <71389724+naveen-colab@users.noreply.github.com>
2020-09-16 21:10:56 +0530

    Update twitter_bot.py

--- twitter_bot.py
+++ twitter_bot.py
@@ -2,7 +2,7 @@ import tweepy
 def authentication():
   consumer_key=os.getenv("consumer_key")
   consumer_secret=os.getenv("consumer_secret")
-  access_token=os.getenv(" access_token")
+  access_token=os.getenv("access_token")
   access_token_secret=os.getenv("access_token_secret")
   
   auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
